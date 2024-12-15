db86d1c15be461014f31f84345047683b8b2de25
AlyazidyDeveloper <AlyazidyProgramming@gmail.com>
2024-12-15 17:21:51 +0300

    Update1

--- ObserverDesignPattern_ProductAndCustomer/obj/Debug/net9.0/ObserverDesignPattern_ProductAndCustomer.AssemblyInfo.cs
+++ ObserverDesignPattern_ProductAndCustomer/obj/Debug/net9.0/ObserverDesignPattern_ProductAndCustomer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObserverDesignPattern_ProductAndCustomer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e4242baa3b3cd9a9b4fc2b4e81fc47515ecc6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f66591bfeb201e2a791a75e138731c0d194cb1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObserverDesignPattern_ProductAndCustomer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObserverDesignPattern_ProductAndCustomer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
